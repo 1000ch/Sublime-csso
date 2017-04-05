376b2a7c390fbfe255505b58d8420143820bae16
1000ch <shogo.sensui@gmail.com>
2017-04-05 11:46:39 +0900

    Use csso instead csso-cli

--- csso.js
+++ csso.js
@@ -1,27 +1,17 @@
 'use strict';
 
-const path = require('path');
-const execa = require('execa');
 const getStdin = require('get-stdin');
-
-const unix = path.normalize(path.join(__dirname, 'node_modules', '.bin', 'csso'));
-const win = path.normalize(path.join(__dirname, 'node_modules', '.bin', 'csso.cmd'));
-const CSSO_BIN = require('os').type() === 'Windows_NT' ? win : unix;
+const csso = require('csso');
 
 getStdin()
-  .then(data => csso(data))
+  .then(data => minify(data))
   .then(data => process.stdout.write(data));
 
-function csso(data) {
+function minify(data) {
   const options = JSON.parse(process.argv[2]);
-  const args = [];
+  const css = Buffer.isBuffer(data) ? data.toString() : data;
 
-  if (!options.restructure) {
-    args.push('--restructure-off');
-  }
-
-  return execa.stdout(CSSO_BIN, args, {
-    encoding: null,
-    input: Buffer.from(data)
-  });
+  return csso.minify(css, {
+    restructure: options.restructure
+  }).css;
 }
