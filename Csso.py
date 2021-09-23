14c879155bd3af8c09d2ec7d47e095fa2434b34d
1000ch <shogosensui@gmail.com>
2021-09-23 14:56:28 +0900

    fix: fix bin path

--- Csso.py
+++ Csso.py
@@ -12,7 +12,7 @@ except:
 sublime.Region.totuple = lambda self: (self.a, self.b)
 sublime.Region.__iter__ = lambda self: self.totuple().__iter__()
 
-BIN_PATH = join(sublime.packages_path(), dirname(realpath(__file__)), 'csso.js')
+BIN_PATH = join(sublime.packages_path(), dirname(realpath(__file__)), 'csso.mjs')
 
 def get_setting(view, key):
     settings = view.settings().get('Csso')
--- csso.mjs
+++ csso.mjs
@@ -1,4 +1,4 @@
-import { Buffer } from 'buffer';
+import {Buffer} from 'buffer';
 import getStdin from 'get-stdin';
 import csso from 'csso';
 
@@ -7,10 +7,10 @@ getStdin()
   .then(data => process.stdout.write(data));
 
 function minify(data) {
-  const options = JSON.parse(process.argv[2]);
+  const {restructure} = JSON.parse(process.argv[2]);
   const css = Buffer.isBuffer(data) ? data.toString() : data;
 
   return csso.minify(css, {
-    restructure: options.restructure
+    restructure
   }).css;
 }
