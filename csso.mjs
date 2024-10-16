4465a3aa9760b52554ed721049637d71d98d568c
1000ch <shogosensui@gmail.com>
2024-10-17 08:20:34 +0900

    Use exported function

--- csso.mjs
+++ csso.mjs
@@ -1,16 +1,16 @@
 import {Buffer} from 'node:buffer';
 import getStdin from 'get-stdin';
-import csso from 'csso';
+import {minify} from 'csso';
 
 getStdin()
-  .then(data => minify(data))
+  .then(data => applyCsso(data))
   .then(data => process.stdout.write(data));
 
-function minify(data) {
+function applyCsso(data) {
   const {restructure} = JSON.parse(process.argv[2]);
   const css = Buffer.isBuffer(data) ? data.toString() : data;
 
-  return csso.minify(css, {
+  return minify(css, {
     restructure
   }).css;
 }
